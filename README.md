cbe85450dc3d1a154570929a48fd787557611ba0
Sergio <sergioperezalamillo@gmail.com>
2020-04-13 11:32:49 +0200

    Subiendo

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# data-analysis-pipeline
+## Proyecto data-analysis-pipeline
+
+En este proyecto vamos a limpiar un csv de musica.
+
+Después de limpiar el csv vamos a añadirle una nueva columna que llamaremos Albums que desde la Api de Deezer buscremos y añadiremos llos albumnes 
--- funciones.py
+++ funciones.py
@@ -1,39 +1,6 @@
-import requests
-import json
 import pandas as pd
-import os
 import sys
-from dotenv import load_dotenv
-load_dotenv()
 import argparse
-def buscarAlbum(albunes):    
-    if albunes["total"] != 0:
-        for elemento in albunes.items():    
-            for elem in elemento:
-                if type(elem) == list:                   
-                    for x,y in elem[0].items():
-                        if x =="album":
-                             for el,busco in y.items():
-                                    if el == 'title':
-                                        return  busco
-    else:    
-        return None
-
-def descargar(df):
-    albunes = []
-    for index,row in df.iterrows():
-        titulo = row.title
-        artista = row.artist
-        artists_url = f"https://api.deezer.com/search?q=artist:'{artista}' track:'{titulo}'"
-        artists_response = requests.get(artists_url)
-        album =artists_response.json()
-        try:
-            albunes.append(buscarAlbum(album))
-        except:
-            albunes.append(None)
-
-    return albunes
-
 
 def argParse(archivo):
     df = pd.read_csv(archivo)
